7cbc0b5c0d89a8d759d62c12d945d26ad32dc3be
MCERQUA <mikecerqua@gmail.com>
2025-07-17 00:01:45 -0400

    Update: 2025-07-17  0:01:45.52

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://gunrangeinsurance.com',
   integrations: [react(), tailwind()],
   vite: {
     optimizeDeps: {
